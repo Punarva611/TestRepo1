85d292f685154a75d3940ca0d7316c4d1ac7ec10
Punarva611 <cpunarva611@gmail.com>
2025-06-06 11:03:09 +0530

    Demo1 commit 2nd time

--- src/Demo1.java
+++ src/Demo1.java
@@ -2,5 +2,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("My first Java program");
         System.out.println("TESTING Purpose");
+        System.out.println("testing 2");
     }
 }
